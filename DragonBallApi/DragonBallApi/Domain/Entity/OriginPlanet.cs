4a202e5fe0cf07305c6219963bf2a13c32724e7d
Gilberto Evangelista Junior - GEV <gilberto.evangelista@iteris.com.br>
2021-10-19 14:17:57 -0300

    Add entity OriginPlanet

---
+++ DragonBallApi/DragonBallApi/Domain/Entity/OriginPlanet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DragonBallApi.Domain.Entity
+{
+    public class OriginPlanet
+    {
+        public string Name { set; get; }
+        public string ImageUrl { set; get; }
+
+    }
+}
