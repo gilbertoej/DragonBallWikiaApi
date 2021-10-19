0ec65c32d8385f96b67a8cc4c5cf8dd1b5f0fb04
Sarah de Oliveira Rico SRI <sarah.rico@iteris.com.br>
2021-10-19 14:35:28 -0300

    criação da entity characterspecies

---
+++ DragonBallApi/DragonBallApi/Domain/Entity/CharacterSpecies.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DragonBallApi.Domain.Entity
+{
+    public class CharacterSpecies
+    {
+        public int Id { get; set; }
+        public int CharacteId { get; set; }
+        public int SpecieId { get; set; }
+    }
+}
